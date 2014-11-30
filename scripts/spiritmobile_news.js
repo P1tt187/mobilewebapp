65941025d387ab58edd587a53c2e09e403e2eef6
fabian <f.markert87@googlemail.com>
2014-11-30 04:55:58 +0100

    add markdown support

--- scripts/spiritmobile_news.js
+++ scripts/spiritmobile_news.js
@@ -216,7 +216,7 @@ var News = function () {
             $('body').append('<div data-role="page" id="newsPage-' + news_id + '"></div>');
 
             //markdown converter
-            var convert = new Markdown.getSanitizingConverter.makeHtml;
+            var convert = new Markdown.getSanitizingConverter().makeHtml;
 
             console.log(convert(this.getValueFromKey('news', news_id)));
             $('#newsPage-' + news_id).append(
